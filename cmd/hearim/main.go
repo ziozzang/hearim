a3915f331278512496b88d3b33df5f754ef3892a
Jioh Jung <ziozzang@gmail.com>
2026-09-21 13:49:06 +0900

    Bump version to 0.2.2 (clean release; v0.2.1 assets hit CDN cache on re-upload)

--- cmd/hearim/main.go
+++ cmd/hearim/main.go
@@ -29,7 +29,7 @@ import (
 	"hearim/internal/hearim/selfupdate"
 )
 
-var version = "0.2.1"
+var version = "0.2.2"
 
 // updateRepo is the GitHub repository self-update pulls release builds from.
 const updateRepo = "ziozzang/hearim"
