44659b8be9fcf7110d439cf58974c5d58f296b26
Jioh Jung <ziozzang@gmail.com>
2026-09-21 16:23:43 +0900

    Bump version to 0.6.3 (v0.6.2 binaries embedded 0.6.1)

--- cmd/hearim/main.go
+++ cmd/hearim/main.go
@@ -29,7 +29,7 @@ import (
 	"hearim/internal/hearim/selfupdate"
 )
 
-var version = "0.6.1"
+var version = "0.6.3"
 
 // updateRepo is the GitHub repository self-update pulls release builds from.
 const updateRepo = "ziozzang/hearim"
