c83fdcc1b7798a1293c1e05125fa98d4d90c05cc
Jioh Jung <ziozzang@gmail.com>
2026-09-21 17:07:39 +0900

    Bump version to 0.6.4 (license attribution in CLI output)

--- cmd/hearim/main.go
+++ cmd/hearim/main.go
@@ -29,7 +29,7 @@ import (
 	"hearim/internal/hearim/selfupdate"
 )
 
-var version = "0.6.3"
+var version = "0.6.4"
 
 // updateRepo is the GitHub repository self-update pulls release builds from.
 const updateRepo = "ziozzang/hearim"
